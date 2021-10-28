aef8bc6a26dba310b88f7cc03d01dbd94ebcf6ec
Luis Ariza <luismanuelariza@hotmail.com>
2021-10-28 13:49:25 -0500

    Estructura de seleccion de casillas

--- tres_en_raya.c
+++ tres_en_raya.c
@@ -5,6 +5,7 @@
 void loop (char c [3][3]);
 void Intro_Primera (char c [3][3]);
 void tablero (char c [3][3]);
+void Intro_Yo (char c [3][3]);
 
 int main ()
 {
@@ -18,6 +19,8 @@ int main ()
 void loop (char c [3][3])
 {
     Intro_Primera (c);
+    tablero (c);
+    Intro_Yo (c);
     tablero (c);
 }
 
@@ -37,6 +40,128 @@ void Intro_Primera (char c [3][3])
     }
 }
 
+void Intro_Yo (char c [3][3])
+{
+    int i, j, k;
+    char aux;
+    do
+    {
+        do
+        {
+            printf ("Coloca una ficha: ");
+            fflush (stdin);
+            scanf ("%c", &aux);
+        } while (aux < '1' || aux > '9');
+
+        k = 0;
+
+        switch (aux)
+        {
+            case '1':
+            {
+                i = 0;
+                j = 0;
+                if (c[i][j] == 'X' || c[i][j] =='O')
+                {
+                    k = 1;
+                    printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+                }       
+                break;
+            }
+            case '2':
+            {
+                i = 0;
+                j = 1;
+                if (c[i][j] == 'X' || c[i][j] =='O')
+                {
+                    k = 1;
+                    printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+                }       
+                break;
+            }
+            case '3':
+            {
+                i = 0;
+                j = 2;
+                if (c[i][j] == 'X' || c[i][j] =='O')
+                {
+                    k = 1;
+                    printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+                }       
+                break;
+            }
+            case '4':
+            {
+                i = 1;
+                j = 0;
+                if (c[i][j] == 'X' || c[i][j] =='O')
+                {
+                    k = 1;
+                    printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+                }       
+                break;
+            }
+            case '5':
+            {
+                i = 1;
+                j = 1;
+                if (c[i][j] == 'X' || c[i][j] =='O')
+                {
+                    k = 1;
+                    printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+                }       
+                break;
+            }
+            case '6':
+            {
+                i = 1;
+                j = 2;
+                if (c[i][j] == 'X' || c[i][j] =='O')
+                {
+                    k = 1;
+                    printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+                }       
+                break;
+            }
+            case '7':
+            {
+                i = 2;
+                j = 0;
+                if (c[i][j] == 'X' || c[i][j] =='O')
+                {
+                    k = 1;
+                    printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+                }       
+                break;
+            }
+            case '8':
+            {
+                i = 2;
+                j = 1;
+                if (c[i][j] == 'X' || c[i][j] =='O')
+                {
+                    k = 1;
+                    printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+                }       
+                break;
+            }
+            case '9':
+            {
+                i = 2;
+                j = 2;
+                if (c[i][j] == 'X' || c[i][j] =='O')
+                {
+                    k = 1;
+                    printf ("La casilla esta ocupada! Intentalo con otro numero!!\n\n");
+                }       
+                break;
+            }
+        }
+    } while (k == 1);
+    
+    c[i][j] = 'X';
+}
+
 void tablero (char c [3][3])
 {
     int i, j;
@@ -47,9 +172,9 @@ void tablero (char c [3][3])
         {
             if (j < 2)
             {
-                printf (" %c |",c[i][j]);
+                printf (" %c |", c[i][j]);
             } else {
-                printf (" %c ",c[i][j]);
+                printf (" %c ", c[i][j]);
             }
         }
         if (i < 2) 
