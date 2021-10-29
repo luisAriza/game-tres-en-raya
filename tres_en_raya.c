c92800ab4fc4c5cee333826d7f9956dc98fd95bf
Luis Ariza <luismanuelariza@hotmail.com>
2021-10-29 11:58:10 -0500

    Se añadio la funcion ganador-perdedor

--- tres_en_raya.c
+++ tres_en_raya.c
@@ -8,6 +8,7 @@ void Intro_Primera (char c[3][3]);
 void tablero (char c[3][3]);
 void Intro_Yo (char c[3][3]);
 void Intro_IA (char c[3][3]);
+int ganador (char c[3][3]);
 
 int main ()
 {
@@ -20,7 +21,7 @@ int main ()
 
 void loop (char c [3][3])
 {
-    int i;
+    int i, j;
     i = 0;
 
     Intro_Primera (c);
@@ -36,9 +37,21 @@ void loop (char c [3][3])
         } else {
             Intro_IA (c);
         }
+        j = ganador (c);
         i++;
 
-    } while (i <= 9);
+    } while (i <= 9 && j == 2);
+
+    if (j == 0)
+    {
+        printf ("Felicidades, Ganaste!!\n\n");
+    } 
+    else if (j == 1)
+    {
+        printf("Mala suerte, has perdido!!\n\n");
+    } else {
+        printf ("Es un empate, intentalo de nuevo!!\n\n");
+    }
 }
 
 void Intro_Primera (char c [3][3])
@@ -220,4 +233,90 @@ void tablero (char c [3][3])
         }
     }
     printf ("\n\n");
+}
+
+int ganador (char c [3][3])
+{
+    if (c[0][0] == 'X' || c[0][0] == 'O')
+    {
+        if (c[0][0] == c[0][1] && c[0][0] == c[0][2])
+        {
+            if (c[0][0] == 'X')
+            {
+                return 0;  //He ganado
+            } else {
+                return 1;  //He perdido
+            }
+        }
+        if (c[0][0] == c[1][0] && c[0][0] == c[2][0])
+        {
+            if (c[0][0] == 'X')
+            {
+                return 0;  //He ganado
+            } else {
+                return 1;  //He perdido
+            }
+        }
+    }
+    if (c[1][1] == 'X' || c[1][1] == 'O')
+    {
+        if (c[1][1] == c[0][0] && c[1][1] == c[2][2])
+        {
+            if (c[1][1] == 'X')
+            {
+                return 0;  //He ganado
+            } else {
+                return 1;  //He perdido
+            }
+        }
+        if (c[1][1] == c[1][0] && c[1][1] == c[1][2])
+        {
+            if (c[1][1] == 'X')
+            {
+                return 0;  //He ganado
+            } else {
+                return 1;  //He perdido
+            }
+        }
+        if (c[1][1] == c[2][0] && c[1][1] == c[0][2])
+        {
+            if (c[1][1] == 'X')
+            {
+                return 0;  //He ganado
+            } else {
+                return 1;  //He perdido
+            }
+        }
+        if (c[1][1] == c[0][1] && c[1][1] == c[2][1])
+        {
+            if (c[1][1] == 'X')
+            {
+                return 0;  //He ganado
+            } else {
+                return 1;  //He perdido
+            }
+        }
+    }
+    if (c[2][2] == 'X' || c[2][2] == 'O')
+    {
+        if (c[2][2] == c[2][0] && c[2][2] == c[2][1])
+        {
+            if (c[2][2] == 'X')
+            {
+                return 0;  //He ganado
+            } else {
+                return 1;  //He perdido
+            }
+        }
+        if (c[2][2] == c[0][2] && c[2][2] == c[1][2])
+        {
+            if (c[2][2] == 'X')
+            {
+                return 0;  //He ganado
+            } else {
+                return 1;  //He perdido
+            }
+        }
+    }
+    return 2;
 }
